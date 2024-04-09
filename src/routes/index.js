334c3133be86c67c6ac9c1cc3fbeb2016c25a1a6
phonglb123 <luongbaphong20041@gmail.com>
2024-04-09 11:24:03 +0700

    u

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import routerProduct from "./product.js";
-import routerAuth from "./auth.js";
+// import routerAuth from "./auth.js";
 import routerCategory from "./category.js";
 import routerImages from "./upload.js";
 const router = express.Router();
 
 router.use("/products", routerProduct);
 router.use("/categories", routerCategory);
-router.use("/auth", routerAuth);
+// router.use("/auth", routerAuth);
 router.use("/images", routerImages);
 export default router;
