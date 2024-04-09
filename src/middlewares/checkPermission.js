df8d6373aa2758306f241006a14a13a01870b176
phonglb123 <luongbaphong20041@gmail.com>
2024-04-09 20:14:00 +0700

    update

--- src/middlewares/checkPermission.js
+++ src/middlewares/checkPermission.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import User from "../models/User";
+import User from "../models/user.js";
 dotenv.config();
 
 const { SECRET_CODE } = process.env;
